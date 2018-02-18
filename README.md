bfd697326acae3e95a4adb25665dd92cf30898b6
Adam Romański <adam.romanski@netguru.pl>
2018-02-18 16:47:52 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,6 @@
+# MOVEOM
+## Move the body. Move the energy. Be moved.
+
+This website was created for a friend of mine who's a great and experienced yoga teacher based in London. Feel free to check the website at http://moveom.uk
+
+This project uses Gatsbyjs (http://gatsbyjs.org) 
---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+/* global __dirname */
+module.exports = {
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-resolve-src',
+    'gatsby-plugin-react-next',
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat\:700`,
+          `'Playfair Display'\:400, 800`,
+        ]
+      }
+    }
+  ],
+};
